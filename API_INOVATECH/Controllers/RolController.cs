927c19fbd97e44044ea5bf645b93931dd233c4fd
BrandonLozada <brandon_lozada@outlook.com>
2023-04-19 00:54:27 -0600

    Elaboración del EndPoint Rol.

---
+++ API_INOVATECH/Controllers/RolController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+using Models;
+using BLL;
+using Models.DTO;
+
+namespace API_INOVATECH.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RolController : ControllerBase
+    {
+        private readonly string Cadena;
+        public RolController(IConfiguration Config)
+        {
+            Cadena = Config.GetConnectionString("PROD");
+        }
+
+        [HttpGet]
+        [Route("ListarTodo")]
+        public IActionResult ListarTodo()
+        {
+            List<RolRepDTO> lstRolRep = BL_ROL.ConsultaRoles(Cadena);
+
+            return Ok(new { Value = lstRolRep });
+        }
+    }
+}
---
+++ BLL/BL_ROL.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BL_ROL
+    {
+        public static List<RolRepDTO> ConsultaRoles(string P_Cadena)
+        {
+            List<RolRepDTO> lstRolRep = new List<RolRepDTO>();
+            DataTable Dt = new DataTable();
+
+            var dpParametros = new
+            {
+                P_Accion = 1
+            };
+
+            Dt = Contexto.Funcion_StoreDB(P_Cadena, "spRoles", dpParametros);
+
+            if (Dt.Rows.Count > 0)
+            {
+                lstRolRep = (from item in Dt.AsEnumerable()
+                                      select new RolRepDTO
+                                      {
+                                          id_rol = item.Field<int>("id_rol"),
+                                          nombre = item.Field<string>("nombre")
+                                      }
+                               ).ToList();
+            }
+
+            return lstRolRep;
+        }
+    }
+}
