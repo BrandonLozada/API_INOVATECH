dad14071a804ff0598845d5c1ab8eaea17650337
BrandonLozada <brandon_lozada@outlook.com>
2023-04-19 00:55:56 -0600

    Elaboración del EndPoint del Puesto.

---
+++ API_INOVATECH/Controllers/PuestoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+using Models;
+using BLL;
+using Models.DTO;
+
+namespace API_INOVATECH.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PuestoController : ControllerBase
+    {
+        private readonly string Cadena;
+        public PuestoController(IConfiguration Config)
+        {
+            Cadena = Config.GetConnectionString("PROD");
+        }
+
+        [HttpGet]
+        [Route("ListarTodo")]
+        public IActionResult ListarTodo()
+        {
+            List<PuestoRepDTO> lstPuestoRep = BL_PUESTO.ConsultaPuestos(Cadena);
+
+            return Ok(new { Value = lstPuestoRep });
+        }
+    }
+}
---
+++ BLL/BL_PUESTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.CodeAnalysis;
+using Models.DTO;
+
+namespace BLL
+{
+    public class BL_PUESTO
+    {
+        public static List<PuestoRepDTO> ConsultaPuestos(string P_Cadena)
+        {
+            List<PuestoRepDTO> lstPuestoRep = new List<PuestoRepDTO>();
+            DataTable Dt = new DataTable();
+
+            var dpParametros = new
+            {
+                P_Accion = 1
+            };
+
+            Dt = Contexto.Funcion_StoreDB(P_Cadena, "spPuestos", dpParametros);
+
+
+            if (Dt.Rows.Count > 0)
+            {
+                lstPuestoRep = (from item in Dt.AsEnumerable()
+                                 select new PuestoRepDTO
+                                 {
+                                     id_puesto = item.Field<int>("id_puesto"),
+                                     nombre = item.Field<string>("nombre")
+                                 }
+                               ).ToList();
+            }
+
+            return lstPuestoRep;
+        }
+    }
+}
