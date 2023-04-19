2d2af34a5c5e36cbb0c50e70736c23805857e903
BrandonLozada <brandon_lozada@outlook.com>
2023-04-19 00:57:09 -0600

    Elaboración del EndPoint del Permiso.

---
+++ API_INOVATECH/Controllers/PermisoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+using Models;
+using BLL;
+using Models.DTO;
+
+namespace API_INOVATECH.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PermisoController : ControllerBase
+    {
+        private readonly string Cadena;
+        public PermisoController(IConfiguration Config)
+        {
+            Cadena = Config.GetConnectionString("PROD");
+        }
+
+        [HttpGet]
+        [Route("ListarTodo")]
+        public IActionResult ListarTodo()
+        {
+            List<PermisoRepDTO> lstPermisoRep = BL_PERMISO.ConsultaPermisos(Cadena);
+
+            return Ok(new { Value = lstPermisoRep });
+        }
+    }
+}
---
+++ BLL/BL_PERMISO.cs
@@ -0,0 +1,41 @@
+﻿using DAL;
+using Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BL_PERMISO
+    {
+        public static List<PermisoRepDTO> ConsultaPermisos(string P_Cadena)
+        {
+            List<PermisoRepDTO> lstPermisoRep = new List<PermisoRepDTO>();
+            DataTable Dt = new DataTable();
+
+            var dpParametros = new
+            {
+                P_Accion = 1
+            };
+
+            Dt = Contexto.Funcion_StoreDB(P_Cadena, "spPermisos", dpParametros);
+
+
+            if (Dt.Rows.Count > 0)
+            {
+                lstPermisoRep = (from item in Dt.AsEnumerable()
+                                      select new PermisoRepDTO
+                                      {
+                                          id_permiso = item.Field<int>("id_permiso"),
+                                          nombre = item.Field<string>("nombre")
+                                      }
+                               ).ToList();
+            }
+
+            return lstPermisoRep;
+        }
+    }
+}
