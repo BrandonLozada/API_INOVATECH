1ad81baf3a46809710c4ed198d71ac6e61ad21a1
Elizabeth Herrera <elizabeth.herrera2530@gmail.com>
2023-04-21 14:22:26 -0600

    Union de DTOs en un solo archivo .cs

--- Models/DTO/DomicilioRepDTO.cs
+++ Models/DTO/DomicilioRepDTO.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace Models.DTO
 {
-    public class DomicilioRepDTO
-    {
-        public string id_usuario { get; set; }
-        public string calle { get; set; }
-        public string? numero_interior { get; set; }
-        public string? numero_exterior { get; set; }
-        public string? entre_calles_1 { get; set; }
-        public string? entre_calles_2 { get; set; }
-        public string codigo_postal { get; set; }
-        public string colonia { get; set; }
-        public string ciudad { get; set; }
-        public string estado { get; set; }
-        public string pais { get; set; }
-    }
+    //public class DomicilioRepDTO
+    //{
+    //    public string id_usuario { get; set; }
+    //    public string calle { get; set; }
+    //    public string? numero_interior { get; set; }
+    //    public string? numero_exterior { get; set; }
+    //    public string? entre_calles_1 { get; set; }
+    //    public string? entre_calles_2 { get; set; }
+    //    public string codigo_postal { get; set; }
+    //    public string colonia { get; set; }
+    //    public string ciudad { get; set; }
+    //    public string estado { get; set; }
+    //    public string pais { get; set; }
+    //}
 }
