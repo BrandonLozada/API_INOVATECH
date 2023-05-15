c21df925415e2f3466bd173b1e28693c7dc202b3
BrandonLozada <brandon_lozada@outlook.com>
2023-05-14 22:21:19 -0600

    Implementar validaciones.

--- API_INOVATECH/Controllers/UsuarioController.cs
+++ API_INOVATECH/Controllers/UsuarioController.cs
@@ -6,7 +6,9 @@ using System.Diagnostics.CodeAnalysis;
 using Models;
 using BLL;
 using Models.DTO;
-
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace API_INOVATECH.Controllers
 {
@@ -22,10 +24,180 @@ namespace API_INOVATECH.Controllers
             Cadena = Config.GetConnectionString("PROD");
         }
 
+        public static bool IsPositive(int number)
+        {
+            return number > 0;
+        }
+
+        public static bool IsNegative(int number)
+        {
+            return number < 0;
+        }
+
+        public static bool IsZero(int number)
+        {
+            return number == 0;
+        }
+
+        private static bool ValidarId(int number)
+        {
+            if (!IsNegative(number) && IsPositive(number) && !IsZero(number))
+                return true;
+           
+            return false;
+        }
+
+        private static bool ValidarFechaNacimiento(string fecha_nacimiento)
+        {
+            DateTime fecha;
+
+            if (DateTime.TryParseExact(fecha_nacimiento, "yyyy/MM/dd", null, System.Globalization.DateTimeStyles.None, out fecha))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool ValidarSexo(string sexo)
+        {
+            Dictionary<string, string> dictSexos = new Dictionary<string, string>
+            {
+                { "M", "Activo" },
+                { "F", "Inactivo" }
+            };
+
+            if (dictSexos.ContainsKey(sexo))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool ValidarCelular(string celular)
+        {
+            Regex r = new Regex(@"^([0-9]){10,10}$");
+            Match match = r.Match(celular);
+
+            if (match.Success)
+                return true;
+            
+            return false;
+        }
+
+        private static bool ValidarCorreo(string correo)
+        {
+            //if (!MailAddress.TryCreate(correo, out var mailAddress))
+            //    return false;
+
+            try
+            {
+                MailAddress m = new MailAddress(correo);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private static bool ValidarContrasenia(string contrasenia)
+        {
+            Regex r = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{10,})");
+            Match match = r.Match(contrasenia);
+
+            if (match.Success)
+            {
+                if (contrasenia.Length > 20)
+                    return false;
+       
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool ValidarActivo(int es_activo)
+        {
+            Dictionary<int, string> dictEstados = new Dictionary<int, string>
+            {
+                { 1, "Activo" },
+                { 0, "Inactivo" }
+            };
+
+            if (dictEstados.ContainsKey(es_activo))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool ValidarRol(int id_rol)
+        {
+            if (id_rol == 1)
+                return false;
+
+            Dictionary<int, string> dictRoles = new Dictionary<int, string>
+            {
+                { 2, "Programador" },
+                { 3, "Administrador de base de datos" },
+                { 4, "Administrador" },
+                { 5, "Soporte de aplicaciones" },
+                { 6, "Encargado de sistemas" },
+                { 7, "Ordinario" }
+            };
+
+            if (dictRoles.ContainsKey(id_rol))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         [HttpPost]
         [Route("GuardarUsuario")]
         public IActionResult GuardarUsuario([FromBody] UsuarioDTO Usuario)
         {
+            if (!ValidarFechaNacimiento(Usuario.fecha_nacimiento))
+            {
+                return BadRequest(new { Value = "Formato inválido para campo fecha. Formato permitido (YYYY/MM/DD)" });
+            }
+
+            if (!ValidarSexo(Usuario.sexo))
+            {
+                return BadRequest(new { Value = "Valor inválido para campo sexo. Valores permitidos 'M' | 'F'" });
+            }
+
+            if (!ValidarCelular(Usuario.celular))
+            {
+                return BadRequest(new { Value = "Valor inválido para campo celular. Valor permitido '##########' de 10 digítos" });
+            }
+
+            if (!ValidarCorreo(Usuario.correo))
+            {
+                return BadRequest(new { Value = "El correo no tiene formato correcto." });
+            }
+
+            if (!ValidarContrasenia(Usuario.contrasenia))
+            {
+                return BadRequest(new { Value = "La contraseña no cumple con los requisitos." });
+            }
+
+            if (!ValidarActivo(Usuario.es_activo))
+            {
+                return BadRequest(new { Value = "Valor inválido para la actividad del usuario." });
+            }
+
+            if (!ValidarRol(Usuario.id_rol))
+            {
+                return BadRequest(new { Value = "No se encuentra ese rol de usuario." });
+            }
+
             List<string> lstDatos = BL_USUARIO.InsertarUsuario(Cadena, Usuario.nombre, Usuario.primer_apellido, Usuario.segundo_apellido, Usuario.fecha_nacimiento, Usuario.sexo, Usuario.celular, Usuario.correo, Usuario.contrasenia, Usuario.es_activo, Usuario.id_rol);
 
             if (lstDatos[0] == "00")
@@ -36,13 +208,52 @@ namespace API_INOVATECH.Controllers
             {
                 return BadRequest(new { Value = lstDatos[1] });
             }
-
         }
 
         [HttpPut]
         [Route("ActualizarUsuario/{IdUsuario}")]
         public IActionResult ActualizarUsuario(int IdUsuario, [FromBody] UsuarioDTO Usuario)
         {
+            if (!ValidarId(IdUsuario))
+            {
+                return BadRequest(new { Value = "Identificador del usuario inválido. No existe usuario con ese identificador" });
+            }
+
+            if (!ValidarFechaNacimiento(Usuario.fecha_nacimiento))
+            {
+                return BadRequest(new { Value = "Formato inválido para campo fecha. Formato permitido (YYYY/MM/DD)" });
+            }
+
+            if (!ValidarSexo(Usuario.sexo))
+            {
+                return BadRequest(new { Value = "Valor inválido para campo sexo. Valores permitidos 'M' | 'F'" });
+            }
+
+            if (!ValidarCelular(Usuario.celular))
+            {
+                return BadRequest(new { Value = "Valor inválido para campo celular. Valor permitido '##########' de 10 digítos" });
+            }
+
+            if (!ValidarCorreo(Usuario.correo))
+            {
+                return BadRequest(new { Value = "El correo no tiene formato correcto." });
+            }
+
+            if (!ValidarContrasenia(Usuario.contrasenia))
+            {
+                return BadRequest(new { Value = "La contraseña no cumple con los requisitos." });
+            }
+
+            if (!ValidarActivo(Usuario.es_activo))
+            {
+                return BadRequest(new { Value = "Valor inválido para la actividad del usuario." });
+            }
+
+            if (!ValidarRol(Usuario.id_rol))
+            {
+                return BadRequest(new { Value = "No se encuentra ese rol de usuario." });
+            }
+
             List<string> lstDatos = BL_USUARIO.ActualizarUsuario(Cadena, IdUsuario, Usuario.nombre, Usuario.primer_apellido, Usuario.segundo_apellido, Usuario.fecha_nacimiento, Usuario.sexo, Usuario.celular, Usuario.correo, Usuario.contrasenia, Usuario.es_activo, Usuario.id_rol);
 
             if (lstDatos[0] == "00")
@@ -60,6 +271,11 @@ namespace API_INOVATECH.Controllers
         [Route("EliminarUsuario/{IdUsuario}")]
         public IActionResult EliminarUsuario(int IdUsuario)
         {
+            if (!ValidarId(IdUsuario))
+            {
+                return BadRequest(new { Value = "Identificador del usuario inválido. No existe usuario con ese identificador" });
+            }
+
             List<string> lstDatos = BL_USUARIO.EliminarUsuario(Cadena, IdUsuario);
 
             if (lstDatos[0] == "00")
@@ -73,12 +289,39 @@ namespace API_INOVATECH.Controllers
 
         }
 
+        //[HttpDelete]
+        //[Route("EliminarUsuarioFisico/{IdUsuario}")]
+        //public IActionResult EliminarUsuarioFisico(int IdUsuario)
+        //{
+        //    if (!ValidarId(IdUsuario))
+        //    {
+        //        return BadRequest(new { Value = "Identificador del usuario inválido. No existe usuario con ese identificador" });
+        //    }
+
+        //    List<string> lstDatos = BL_USUARIO.EliminarUsuarioFisico(Cadena, IdUsuario);
+
+        //    if (lstDatos[0] == "00")
+        //    {
+        //        return Ok(new { Value = lstDatos[1] });
+        //    }
+        //    else
+        //    {
+        //        return BadRequest(new { Value = lstDatos[1] });
+        //    }
+
+        //}
+
         // TODO: En los EndPoints donde obtengo a un solo usuario por medio de un ID, la respuesta tiene que ser de otro tipo que no sea "List" (Pref Objeto). 
         //       Esto es para mejor manipulación en el Front y a la hora de la función Modificar enviar como "Props" a otra página.
         [HttpGet]
         [Route("ListarPerfil/{IdUsuario}")]
         public IActionResult ListarPerfil(int IdUsuario)
         {
+            if (!ValidarId(IdUsuario))
+            {
+                return BadRequest(new { Value = "Identificador del usuario inválido. No existe usuario con ese identificador" });
+            }
+
             List<UsuarioBioDTO> lstUsuarioRep = BL_USUARIO.ConsultaPerfil(Cadena, IdUsuario);
 
             return Ok(new { Value = lstUsuarioRep });
